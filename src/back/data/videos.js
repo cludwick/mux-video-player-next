011f45984c13592b199686b7f5e4c08df683a348
Chance Ludwick <cludwick@superiorlivestock.com>
2022-11-16 08:14:27 -0600

    update folder structure

---
+++ src/back/data/videos.js
@@ -0,0 +1,18 @@
+const videos = [
+  {
+    lot: 1,
+    playbackId: 'lyazvkjU2r5018u00gNm1JwZ5cubp5JPT5jfUMQxkO00D4',
+  },
+  {
+    lot: 2,
+    playbackId: 'd01sumCEZUWfx4p9dItWkURbGckImR8Plc02YwYGKExVo',
+  },
+  {
+    lot: 4,
+    playbackId: 'OTtQJV2vseRf02MF6Z7LlVQzeRYPcogUpH3HRdfBnsxE',
+  },
+  {
+    lot: 5,
+    playbackId: 'Y4fAGAjXkECyGp00bHI5Q7d7lQXTa3fI4orj3SdxWuCk',
+  },
+];
