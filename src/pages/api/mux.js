7f518da53f6b581c0d76c3f398bf3c2268957198
Chance Ludwick <cludwick@superiorlivestock.com>
2022-12-13 14:58:06 -0600

    cleaning

--- src/pages/api/mux.js
+++ src/pages/api/mux.js
@@ -1,14 +1,3 @@
-import excuteQuery from '../../app/services/db';
-
-export const sendToDB = async (lotNumber, playbackId) => {
-  try {
-    const result = await excuteQuery(`INSERT INTO \`mux_playback\` (\`lot\`, \`playbackId\`) VALUES ('${lotNumber}', '${playbackId}')`);
-    console.log('Posted to MySQL');
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export default async function handler(req, res) {
   const requestType = req.body.type;
 
